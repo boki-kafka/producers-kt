47d2e74a2da5ac99e509914bae889a2281cdb9da
boki <lsb530@naver.com>
2024-09-13 01:23:03 +0900

    async producer with key(kt)

---
+++ src/main/kotlin/SimpleProducerASyncWithKey.kt
@@ -0,0 +1,67 @@
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.apache.kafka.clients.producer.Callback
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig.*
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
+import org.apache.kafka.common.serialization.StringSerializer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.lang.Exception
+import java.util.*
+
+class SimpleProducerASyncWithKey {
+
+    companion object {
+
+        private val logger = KotlinLogging.logger { }
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val props = initProducerProps()
+
+            val topic = "multipart-topic"
+
+            val producer = KafkaProducer<String, String>(props)
+
+            for (seq in 0..<20) {
+                val record = ProducerRecord(
+                    topic,
+                    seq.toString(),
+                    "hello world $seq"
+                )
+
+                producer.send(record) { metadata, exception ->
+                    if (exception == null) {
+                        logger.info {
+                            """###### record metadata received #####
+                                        |topic: ${metadata?.topic()}
+                                        |partition: ${metadata?.partition()}
+                                        |offset: ${metadata?.offset()}
+                                        |timestamp: ${metadata?.timestamp()}
+                                    """.trimMargin()
+                        }
+                    } else {
+                        logger.error { "exception error from broker ${exception.message}" }
+                    }
+                }
+            }
+
+
+            Thread.sleep(3000L)
+
+            producer.close()
+        }
+
+        private fun initProducerProps(): Properties {
+            val props = Properties().apply {
+                // bootstrap.servers, key.serializer.class, value.serializer.class
+                put(BOOTSTRAP_SERVERS_CONFIG, "10.211.55.53:9092")
+                put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
+                put(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
+            }
+            return props
+        }
+    }
+
+}
